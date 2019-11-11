812cdd1c01af7e62a4c3dfcfae2b78a182a4e77a
Thomas BESSOU <thomas.bessou@hotmail.fr>
2019-11-11 02:57:56 +0100

    Easy stdout panics

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,10 @@ pub mod output_writer;
 
 pub use input_reader::InputReader;
 pub use output_writer::OutputWriter;
+
+pub fn stdout_panics() {
+	std::panic::set_hook(Box::new(|panic_info| {
+		println!("{}", panic_info);
+		std::process::exit(102);
+	}));
+}
